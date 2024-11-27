cd3b78111b2f5d99a6228260898c2a0d252d8028
Hardy <hardsyldev@gmail.com>
2024-11-27 08:09:36 +0300

    bug scroll

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(scrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const footer = document.querySelector(".footer");
+  const lastCard = document.querySelector(".card.scroll");
+  const pinnedSections = gsap.utils.toArray(".pinned");
+
+  pinnedSections  .forEach((section, index, sections) => {
+    let img = section.querySelector(".img");
+
+    let nextSection = sections[index + 1] || lastCard;
+    let endScalePoint = `top+=${nextSection.offsetTop - section.offsetTop} top`;
+
+    gsap.to(section, {
+      scrollTrigger: {
+        trigger: section,
+        start: "top top",
+        end:
+          index === sections.length
+            ? `+=${lastCard.offsetHeight / 2}`
+            : footer.offsetTop - window.innerHeight,
+        pin: true,
+        pinSpacing: false,
+        scrub: 1,
+      },
+    });
+  });
+});
